8c1afb0c342505e7dded3acc3f15c3f9ad649c05
Michael Brade <brade@kde.org>
2014-09-07 14:26:48 +0200

    yo derby:component d-select2

---
+++ index.js
@@ -0,0 +1,15 @@
+module.exports = Select2;
+
+function Select2(){}
+
+Select2.prototype.view = __dirname;
+Select2.prototype.style =__dirname;
+Select2.prototype.name = 'd-select2';
+
+Select2.prototype.init = function(){
+
+};
+
+Select2.prototype.create = function(){
+
+};
