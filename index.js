7c86e99a59d314ca2f0695ce3cde82d5bf5e11fc
Michael Brade <brade@kde.org>
2015-02-22 23:44:20 +0100

    use js and html from d-comp-palette, it does seem to work
    TODO: try the previous one as well, see which one is better!

--- index.js
+++ index.js
@@ -14,249 +14,175 @@ Select2.prototype.name = 'd-select2';
 Select2.prototype.init = function(model) {
 };
 
-Select2.prototype.create = function(model, dom) {
 
+// value - the target property, could be: an object or a primitive type, an array of the objects or primitive types
+// If 'obj' attribute exists it means the target propety is a object
+// If 'key' attribute exists it means the items is a list of objects
+// text - the name of the displayed property
+// single - one item could be choosed only
+// fixed - don't allow add new items
+
+Select2.prototype.create = function (model, dom) {
     require('select2');
+    $ = jQuery;
 
     if (typeof jQuery.fn.select2 === 'undefined') {
         return console.log('select2.jquery.js required to run select2');
     }
 
-    var self = this
-      , defaults = model.get('defaults') || {}
-      , optfns = [
-            'changefn'
-          , 'containercssfn'
-          , 'containercssclassfn'
-          , 'createsearchchoicefn'
-          , 'dropdowncssfn'
-          , 'dropdowncssclassfn'
-          , 'escapemarkupfn'
-          , 'formatinputtooshortfn'
-          , 'formatnomatchesfn'
-          , 'formatresultfn'
-          , 'formatresultcssfn'
-          , 'formatresultcssclassfn'
-          , 'formatsearchingfn'
-          , 'formatselectionfn'
-          , 'formatselectiontoobigfn'
-          , 'idfn'
-          , 'initselectionfn'
-          , 'matcherfn'
-          , 'maximumselectionsizefn'
-          , 'queryfn'
-          , 'sortresultsfn'
-          , 'tokenizerfn'
-        ]
-      , opts = [
-            'ajax'
-          , 'allowclear'
-          , 'close'
-          , 'closeonselect'
-          , 'container'
-          , 'containercss'
-          , 'containercssclass'
-          , 'data'
-          , 'dataval'
-          , 'destroy'
-          , 'disable'
-          , 'dropdowncss'
-          , 'dropdowncssclass'
-          , 'enable'
-          , 'inputtype'
-          , 'loadmorepadding'
-          , 'maximuminputlength'
-          , 'maximumselectionsize'
-          , 'minimuminputlength'
-          , 'minimumresultsforsearch'
-          , 'multiple'
-          , 'onsortend'
-          , 'onsortstart'
-          , 'open'
-          , 'openonenter'
-          , 'openonselect'
-          , 'placeholder'
-          , 'selectonblur'
-          , 'separator'
-          , 'tags'
-          , 'tokenseparators'
-          , 'width'
-          , 'val'
-        ].concat(optfns);
+    var self = this;
 
-    function unrecognized(param) {
-        if (!_.contains(opts)) {
-            console.log('unrecognized parameter "' + key + '"');
+    self.key = model.get('key');
+    self.text = model.get('text');
+    self.obj = model.get('obj');
+    self.single = model.get('single');
+    self.fixed = model.get('fixed');
+
+    // localization
+    var noMatches = self.model.get('noMatches') || self.model.get('lang.select2.noMatches') || "No matches";
+    var onlyOneValue = self.model.get('onlyOneValue') || self.model.get('lang.select2.onlyOneValue') || "You cannot select any more choices";
+
+    // initialization
+    self.$element = $(self.input);
+    var options = {
+        formatNoMatches: function (term) {
+            return noMatches;
+        },
+        formatSelectionTooBig: function (maxSize) {
+            return onlyOneValue;
+        },
+        tags: function () {
+            var items = model.get('items');
+            items = _.map(items, function (item) {
+                return self.key ? {id: item[self.key], text: item[self.text]} : {id: item, text: item};
+            });
+            return items || [];
         }
-    }
-
-    function isBlank(str) {
-        //    return (!str || /^\s*$/.test(str)); //  !/\S/.test(str)
-        return (!str || !str.trim());
-    }
-
-    _.keys(model.get(), unrecognized);
-    _.keys(defaults, unrecognized);
-
-    // convert option values to scoped models
-    opts = _.object(opts, _.map(opts, function(opt) {
-        return model.at(opt);
-    }));
-
-    // set option defaults
-    _.each(_.keys(defaults), function(key) {
-        var val = defaults[key];
-        if (typeof val === 'undefined') return;
-        if (!opts[key]) return;
-        if (typeof opts[key].get() !== 'undefined') return;
-        if (_.isFunction(val)) return (opts[key] = val);
-        opts[key].set(val);
-    });
-
-    // resolve function option names to their actual functions
-    _.each(optfns, function(optfn) {
-        if (_.isFunction(opts[optfn])) return;
-
-        var name = opts[optfn].get();
-        var nofn = function() { return ''; };
-
-        opts[optfn] = undefined;
-        if (typeof name === 'undefined') return;
-        if (isBlank(name)) return (opts[optfn] = nofn);
-        // TODO    if (DERBY.app[name]) return (opts[optfn] = DERBY.app[name]);
-        console.log('cannot find function "' + name + '"');
-    });
-
-    // infer input type if it is not specified
-    if (typeof opts.inputtype.get() === 'undefined') {
-        var requireHidden =
-            opts.ajax.get() ||
-            opts.data.get() ||
-            opts.queryfn;
-        opts.inputtype.set(requireHidden ? 'hidden' : 'select');
-    }
-
-    // infer if select allows multiple values
-    if (typeof opts.multiple.get() === 'undefined') {
-        if (opts.closeonselect.get()) opts.multiple.set(true);
-    }
-
-    var isHidden = opts.inputtype.get() === 'hidden'
-      , el = isHidden ? this.input : this.select;
-
-    // parameters to pass to $.fn.select2
-    var params = {
-          ajax: opts.ajax.get()
-        , allowClear: opts.allowclear.get()
-        , containerCss: opts.containercssfn || opts.containercss.get()
-        , containerCssClass: opts.containercssclassfn || opts.containercssclass.get()
-        , createSearchChoice: opts.createsearchchoicefn
-        , data: opts.data.get()
-        , dropdownCss: opts.dropdowncssfn || opts.dropdowncss.get()
-        , dropdownCssClass: opts.dropdowncssclassfn || opts.dropdowncssclass.get()
-        , escapeMarkup: opts.escapemarkupfn
-        , formatInputTooShort: opts.formatinputtooshortfn
-        , formatNoMatches: opts.formatnomatchesfn
-        , formatResult: opts.formatresultfn
-        , formatResultCss: opts.formatresultcssfn
-        , formatResultCssClass: opts.formatresultcssclassfn
-        , formatSearching: opts.formatsearchingfn
-        , formatSelection: opts.formatselectionfn
-        , formatSelectionTooBig: opts.formatselectiontoobigfn
-        , id: opts.idfn
-        , initSelection: opts.initselectionfn
-        , maximumInputLength: opts.maximuminputlength.get()
-        , maximumSelectionSize: opts.maximumselectionsizefn || opts.maximumselectionsize.get()
-        , minimumInputLength: opts.minimuminputlength.get()
-        , minimumResultsForSearch: opts.minimumresultsforsearch.get()
-        , matcher: opts.matcherfn
-        , multiple: isHidden ? opts.multiple.get() : undefined
-        , openOnEnter: opts.openonenter.get()
-        , openOnSelect: opts.openonselect.get()
-        , placeholder: opts.placeholder.get()
-        , query: opts.queryfn
-        , separator: opts.separator.get()
-        , selectOnBlur: opts.selectonblur.get()
-        , sortResults: opts.sortresultsfn
-        , tags: opts.tags.get()
-        , tokenizer: opts.tokenizerfn
-        , tokenSeparators: opts.tokenseparators.get()
-        , width: opts.width.get()
     };
 
-    jQuery(function () {
-        var datavalSet = valSet = false;
-console.log("PARAMS:", params);
+    if (self.single)
+        options.maximumSelectionSize = 1;
 
-        el = jQuery(el).select2(params);
-        opts.container.set(el.select2('container'));
+    if (self.fixed)
+        options.createSearchChoice = function () {
+            return null;
+        };
 
-        // allow binding to the change event
-        el.on('change', function (e) {
-            datavalSet = valSet = true;
-            opts.val.set(el.select2('val'));
-            opts.dataval.set(el.select2('data'));
-            self.emit('change', e);
-        });
+    self.$element.select2(options);
 
-        // allow binding to the open event
-        el.on('open', function () {
-            self.emit('open');
-        });
+    self.internalChange = false;
 
-        function setDataval(val) {
-            // ensure we aren't responding to a set event we just triggered
-            if (datavalSet) return (datavalSet = false);
-            el.select2('data', val);
-            datavalSet = valSet = true;
-            opts.dataval.set(el.select2('data'));
-            opts.val.set(el.select2('val'));
-        }
-
-        function setVal(val) {
-            // ensure we aren't responding to a set event we just triggered
-            if (valSet) return (valSet = false);
-            el.select2('val', val);
-            datavalSet = valSet = true;
-            opts.dataval.set(el.select2('data'));
-            opts.val.set(el.select2('val'));
-        }
-
-        if (opts.dataval.get())
-            setDataval(opts.dataval.get());
-        else if (opts.val.get())
-            setVal(opts.val.get());
-
-        opts.dataval.on('set', setDataval);
-        opts.val.on('set', setVal);
-
-        opts.close.on('set', function (close) {
-            el.select2(close ? 'close' : 'open');
-        });
-
-        opts.destroy.on('set', function (destroy) {
-            if (destroy) el.select2('destroy');
-        });
-
-        opts.disable.on('set', function (disable) {
-            el.select2(disable ? 'disable' : 'enable');
-        });
-
-        opts.enable.on('set', function (enable) {
-            el.select2(enable ? 'enable' : 'disable');
-        });
-
-        opts.onsortstart.on('set', function (onSortStart) {
-            if (onSortEnd) el.select2('onSortEnd');
-        });
-
-        opts.onsortstart.on('set', function (onsortstart) {
-            if (onSortStart) el.select2('onSortStart');
-        });
-
-        opts.open.on('set', function (open) {
-            el.select2(open ? 'open' : 'close');
-        });
+    // update control
+    model.on("change", "value", function (newVal, oldVal, passed) {
+        self.setValue(newVal);
     });
 
+    // update model
+    self.$element.on("change", function (e) {
+        if (self.internalChange) return;
+        self.internalChange = true;
+        try {
+            if (e.added) {
+                var items = model.get('items');
+                var lowerText = e.added.text.toLowerCase();
+                var itemExists = false;
+
+                // ckeck if the item exists in the collection, if not - add it
+                if (self.key) {
+                    for (var i = 0, cnt = items.length; i < cnt; i++) {
+                        var item = items[i];
+                        if (item[self.text].toLowerCase() === lowerText) {
+                            itemExists = true;
+                            break;
+                        }
+                    }
+                } else {
+                    for (var j = 0, cnt1 = items.length; j < cnt1; j++) {
+                        var item1 = items[j];
+                        if (item1.toLowerCase() === lowerText) {
+                            itemExists = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!itemExists) {
+                    var newItem;
+                    if (self.key) {
+                        newItem = {};
+                        newItem[self.key] = e.added.id;
+                        newItem[self.text] = e.added.text;
+                    } else {
+                        newItem = e.added.text;
+                    }
+                    model.push('items', newItem);
+                }
+            }
+
+            // update model
+            var data = self.$element.select2('data');
+            if (data && data.length > 0) {
+                data = _.map(data, function (item) {
+                    if (self.key) {
+                        var value = {};
+                        value[self.key] = item.id;
+                        value[self.text] = item.text;
+                        return value;
+                    }
+                    return item.text;
+                });
+                if (self.single) {
+                    data = self.obj ? data[0] : (self.key ? data[0][self.key] : data[0]);
+                }
+                else {
+                    data = self.obj ? data : _.map(data, self.key);
+                }
+                model.set('value', data);
+            } else {
+                model.set('value', null);
+            }
+        }
+        finally {
+            self.internalChange = false;
+        }
+    });
+
+    // set initial value
+    var value = model.get('value');
+    self.setValue(value);
+};
+
+Select2.prototype.setValue = function (value) {
+    var self = this;
+    if (self.internalChange) return;
+    if (value) {
+        self.internalChange = true;
+
+        var data;
+        if (self.obj) {
+            if (self.single) {
+                data = [
+                    {id: value[self.key], text: value[self.text]}
+                ];
+            } else {
+                data = _.map(value, function (item) {
+                    return {id: item[self.key], text: item[self.text]};
+                });
+            }
+        } else {
+            if (self.single) {
+                data = [
+                    {id: value, text: value}
+                ];
+            } else {
+                data = _.map(value, function (item) {
+                    return {id: item, text: item};
+                });
+            }
+        }
+
+        self.$element.select2('data', data);
+
+        self.internalChange = false;
+    }
 };
